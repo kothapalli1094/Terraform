c5e78bdd9a7964acbc6bcac442e108a58cfb9bce
kothapalli1094 <shivaramakrishna1094@gmail.com>
2025-09-16 21:10:21 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,11 +2,11 @@
 variable "filename" {
   description = "The name of the file to create"
   type        = string
-  default     = "Devops.txt"
+  default     = "shiva.txt"
 }
 
 variable "content" {
   description = "The content to put inside the file"
   type        = string
-  default     = "Hello guys welcome to  Devops Cloud"
+  default     = "Hello shiva welcome"
 }
