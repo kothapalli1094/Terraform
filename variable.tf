24a3b481c223097cf8017627283304b706d12c6f
kothapalli1094 <shivaramakrishna1094@gmail.com>
2025-09-17 18:15:57 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,11 +2,11 @@
 variable "filename" {
   description = "The name of the file to create"
   type        = string
-  default     = "shiva.txt"
+  default     = "myinfo.txt"
 }
 
 variable "content" {
   description = "The content to put inside the file"
   type        = string
-  default     = "Hello shiva welcome"
+  default     = "This file contains my info"
 }
